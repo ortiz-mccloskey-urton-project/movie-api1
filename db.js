8a2bb610f3f0621d40b74729e9fbd53b6f513ca1
leo-ortiz22 <101903651+leo-ortiz22@users.noreply.github.com>
2022-06-01 11:59:41 -0500

    adding url

---
+++ db.js
@@ -0,0 +1,20 @@
+const baseURL = "https://rogue-marked-gondola.glitch.me/movies/"
+
+const modal = {
+    all: document.querySelector("#modal"),
+    main: document.querySelector("#modal > main"),
+    head: document.querySelector("#modal > header"),
+    foot: document.querySelector("#modal > footer"),
+    container: document.querySelector("#modal-container") // represents the background
+}
+
+const fetchSettings = {
+    method: "GET"
+}
+
+
+    fetch(baseURL + "?title=" , fetchSettings )
+        .then(res => res.json())
+        .then(res => {
+            console.log("res:", res)
+        })
