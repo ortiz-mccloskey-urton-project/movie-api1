783fddec2101f7d39f4a47ff7f79fa1d135e2270
“Cory <coryurton@gmail.com>
2022-06-01 21:56:13 -0500

    trying to add preloader

---
+++ assets.js
@@ -0,0 +1,5 @@
+// const preloader = document.querySelector('.preloader')
+// //listen for the load event
+// window.addEventListener('load', () => {
+//     preloader.classList.add('hide-preloader')
+// })
--- db.js
+++ db.js
@@ -1,7 +1,6 @@
 const baseURL = "https://rogue-marked-gondola.glitch.me/movies"
 
 
-
 // Headers this application uses across the board.
 const customHeaders = new Headers({
     'Content-Type': 'application/json'
@@ -14,28 +13,16 @@ const fetchSettings = {
 }
 
 
+fetch(baseURL + "?_start=14&_limit=15", fetchSettings)
+    .then(res => res.json())
+    .then(res => {
+        console.log("res:", res)
 
 
+        document.getElementById("movies").innerHTML +=
+            res.map((item) => item.title).join("<br>");
 
-
-    fetch(baseURL + "?_start=14&_limit=15", fetchSettings )
-        .then(res => res.json())
-        .then(res => {
-            console.log("res:", res)
-
-
-            document.getElementById("movies").innerHTML +=
-                res.map((item) => item.title).join("<br>");
-
-        })
-
-
-
-
-
-
-
-
+    })
 
 
 // const modal = {
